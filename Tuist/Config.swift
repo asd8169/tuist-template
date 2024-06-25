d354932c06aefb9c6a3389e70dacd39e3309bf97
Inwoo Park <75767746+asd8169@users.noreply.github.com>
2024-06-25 23:14:53 +0900

    Revert "refactor: Tuist Plugin 생성 및 설정 (#6)"

--- Tuist/Config.swift
+++ Tuist/Config.swift
@@ -1,9 +1,5 @@
 import ProjectDescription
 
 let config = Config(
-    plugins: [
-        .local(path: .relativeToRoot("Plugin/DependencyPlugin")),
-        .local(path: .relativeToRoot("Plugin/EnvironmentPlugin")),
-        .local(path: .relativeToRoot("Plugin/TemplatePlugin"))
-    ]
+    
 )
