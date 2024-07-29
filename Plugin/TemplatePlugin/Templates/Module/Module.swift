59fd0cfd21b36fff72b59a163e57bf1b47228795
Inwoo Park <asd8169@icloud.com>
2024-07-30 05:25:42 +0900

    feat: 템플릿 확장 (#21)
    - Module 템플릿 확장: Module.swift에 hasInterface, hasTesting, hasTests, hasExample 속성 추가 및 Project.stencil에서 속성에 따라 타겟(tma target)을 조건부로 포함하도록 수정
    - Example 템플릿 추가: Example.swift 추가, AppDelegate.stencil, ExampleResources.stencil, ExampleSources.stencil 파일 추가
    - Interface 템플릿 추가: Interface.swift 추가, Interface.stencil 파일 추가
    - Testing 템플릿 추가: Testing.swift 추가, Testing.stencil 파일 추가
    - Tests 템플릿 추가: Tests.swift 추가, Tests.stencil 파일 추가

--- Plugin/TemplatePlugin/Templates/Module/Module.swift
+++ Plugin/TemplatePlugin/Templates/Module/Module.swift
@@ -9,42 +9,43 @@ import ProjectDescription
 // scaffold 명령어 시 받을 인자.
 private let nameAttribute = Template.Attribute.required("name")
 private let layerAttribute = Template.Attribute.required("layer")
-private let targetAttribute = Template.Attribute.required("target")
-private let author: Template.Attribute = .required("author")
-private let currentDate: Template.Attribute = .required("currentDate")
+private let authorAttribute: Template.Attribute = .required("author")
+private let currentDateAttribute: Template.Attribute = .required("currentDate")
+private let hasInterfaceAttribute: Template.Attribute = .optional("hasInterface", default: "false")
+private let hasTestingAttribute: Template.Attribute = .optional("hasTesting", default: "false")
+private let hasTestsAttribute: Template.Attribute = .optional("hasTests", default: "false")
+private let hasExampleAttribute: Template.Attribute = .optional("hasExample", default: "false")
 
 private let template = Template(
     description: "A template for a new module",
     attributes: [
         layerAttribute,
         nameAttribute,
-        targetAttribute,
-        author,
-        currentDate
+        authorAttribute,
+        currentDateAttribute,
+        hasInterfaceAttribute,
+        hasTestingAttribute,
+        hasTestsAttribute,
+        hasExampleAttribute
     ],
     items: ModuleTemplate.allCases.map { $0.item }
 )
 
+
+
 enum ModuleTemplate: CaseIterable {
     case project
     case sources
-
+    
     // 템플릿 내부에 추가 파일.
     var item: Template.Item {
         switch self {
         case .project:
-            return .file(path: .basePath + "/Project.swift", templatePath: "Project.stencil")
-
+            return .file(path: "Projects/\(layerAttribute)/\(nameAttribute)/Project.swift", templatePath: "Project.stencil")
         case .sources:
-            return .file(path: .basePath + "/Sources/Source.swift", templatePath: "Sources.stencil")
+            return .file(path: "Projects/\(layerAttribute)/\(nameAttribute)/Sources/Sources.swift", templatePath: "Sources.stencil")
         }
     }
 }
 
 
-// 기본 경로.
-extension String {
-    static var basePath: Self {
-        return "Projects/\(layerAttribute)/\(nameAttribute)"
-    }
-}
